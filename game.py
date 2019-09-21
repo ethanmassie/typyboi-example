e86966b892308d9c64f5916a88e5203dd2bbf36c
Ethan Massie <ethan.massie1@gmail.com>
2019-09-21 17:53:33 -0500

    updated main loop to only modify the player when they move

--- game.py
+++ game.py
@@ -1,17 +1,19 @@
 from typyboi import player, items, tiles, world
 
-_player = None
-
 def main():
     while True:
-        room = world.get_tile(_player.location_x, _player.location_y)
-        room.modify_player(_player)
-        if(_player.is_alive()):
-            print('Choose an actions:\n')
+        if _player.moved:
+            room = world.get_tile(_player.location_x, _player.location_y)
+            room.modify_player(_player)
+            _player.moved = False
+        if _player.is_alive() and not _player.victory:
+            print(room.flavor_text)
+            print('Choose an action:\n')
             available_actions = room.available_actions()
             for action in available_actions:
                 print(action)
             action_input = input('Action: ')
+            print()
             for action in available_actions:
                 if action_input == action.hotkey:
                     _player.do_action(action, **action.kwargs)
@@ -26,4 +28,8 @@ if __name__ == '__main__':
     world.add_tile(tiles.MapTile(0, 0, "First room"))
     world.add_tile(tiles.LootRoom(1, 0, "East loot room", [], 5))
     _player = player.Player(0, 0 , 100)
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        exit(0)
