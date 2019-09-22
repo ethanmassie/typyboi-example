f5a1b3796a6884643e2c402b4add1dd25acd6439
Ethan Massie <ethan.massie1@gmail.com>
2019-09-22 17:42:17 -0500

    moved player definition to game loop method

--- game.py
+++ game.py
@@ -1,25 +1,30 @@
-from typyboi import world
+from typyboi.world import World
 from typyboi.tiles import MapTile, LootRoom, EnemyRoom
 from typyboi.player import Player
 import factory
 
-def main():
+def game_loop():
+    player = Player(0, 0, 100)
+    world = World()
+    world.add_tile(MapTile(0, 0, "First room"))
+    world.add_tile(LootRoom(1, 0, "East loot room", [factory.dagger()], 5))
+    world.add_tile(EnemyRoom(1, 1, 'A spider room', factory.spider() ))
     while True:
-        if _player.moved:
-            room = world.get_tile(_player.location_x, _player.location_y)
-            room.modify_player(_player)
+        if player.moved:
+            room = world.get_tile(player.location_x, player.location_y)
+            room.modify_player(player)
             print(room.flavor_text)
-            _player.moved = False
-        if _player.is_alive() and not _player.victory:
+            player.moved = False
+        if player.is_alive() and not player.victory:
             print('Choose an action:\n')
-            available_actions = room.available_actions()
+            available_actions = room.available_actions(world)
             for action in available_actions:
                 print(action)
             action_input = input('Action: ')
             print()
             for action in available_actions:
                 if action_input == action.hotkey:
-                    _player.do_action(action, **action.kwargs)
+                    player.do_action(action, **action.kwargs)
                     break
         else:
             break
@@ -27,12 +32,8 @@ def main():
 
 
 if __name__ == '__main__':
-    world.add_tile(MapTile(0, 0, "First room"))
-    world.add_tile(LootRoom(1, 0, "East loot room", [factory.dagger()], 5))
-    world.add_tile(EnemyRoom(1, 1, 'A spider room', factory.spider() ))
-    _player = Player(0, 0 , 100)
     try:
-        main()
+        game_loop()
     except KeyboardInterrupt:
         print()
         exit(0)
