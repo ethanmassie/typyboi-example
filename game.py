48b0f2de0bdbd18c94152cf2f0d0b3827af41a0c
Ethan Massie <ethan.massie1@gmail.com>
2019-09-22 16:28:01 -0500

    cleaned up output

--- game.py
+++ game.py
@@ -8,9 +8,9 @@ def main():
         if _player.moved:
             room = world.get_tile(_player.location_x, _player.location_y)
             room.modify_player(_player)
+            print(room.flavor_text)
             _player.moved = False
         if _player.is_alive() and not _player.victory:
-            print(room.flavor_text)
             print('Choose an action:\n')
             available_actions = room.available_actions()
             for action in available_actions:
@@ -29,7 +29,7 @@ def main():
 if __name__ == '__main__':
     world.add_tile(MapTile(0, 0, "First room"))
     world.add_tile(LootRoom(1, 0, "East loot room", [factory.dagger()], 5))
-    world.add_tile(EnemyRoom(1, 1, 'A spider room', 'A spider attacks', factory.spider() ))
+    world.add_tile(EnemyRoom(1, 1, 'A spider room', factory.spider() ))
     _player = Player(0, 0 , 100)
     try:
         main()
